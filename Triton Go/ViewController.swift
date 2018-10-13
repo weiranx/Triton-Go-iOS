b43edef9edc1593f1a2813c03501ede24f2a4099
Weiran Xiong <weiranxiong@gmail.com>
2018-10-12 20:59:37 -0700

    Initial Commit

---
+++ Triton Go/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Triton Go
+//
+//  Created by Weiran Xiong on 10/12/18.
+//  Copyright © 2018 Weiran Xiong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
