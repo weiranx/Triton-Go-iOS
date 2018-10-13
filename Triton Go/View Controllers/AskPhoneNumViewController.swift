3f6df4281f459c5fca4ffd743c94789da74c6843
Weiran Xiong <weiranxiong@gmail.com>
2018-10-13 01:22:38 -0700

    ask phone num

---
+++ Triton Go/View Controllers/AskPhoneNumViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AskPhoneNumViewController.swift
+//  Triton Go
+//
+//  Created by Weiran Xiong on 10/13/18.
+//  Copyright © 2018 Weiran Xiong. All rights reserved.
+//
+
+import UIKit
+
+class AskPhoneNumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
